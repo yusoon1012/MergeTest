4d02767474777813042aa77394d352132898e10b
DESKTOP-2OODV3L\KGA <Va11>
2023-08-03 15:01:19 +0900

    Add.Spare

--- testProject/Assets/Scripts/Hello.cs
+++ testProject/Assets/Scripts/Hello.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class Hello : MonoBehaviour
 {
+    public int Plus;
     public int number = 1;
+
+    public int number22 = 2;
     
     // Start is called before the first frame update
     void Start()
@@ -15,6 +18,7 @@ public class Hello : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Plus = number* number22;
+        Debug.LogFormat("{0}", Plus);
     }
 }
